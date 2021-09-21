98b9cf8cc97a26bb32c034cfec5886ae723e6244
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-09-21 20:32:35 +0200

    Add html template file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component() {
   const element = document.createElement("div");
 
   // Lodash imported by script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["A Webpack basic configuration exercise"], " ");
 
   return element;
 }
